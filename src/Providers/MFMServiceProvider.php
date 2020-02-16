a0e15a163e79bde84d85f63c3f8dce61ff40eac8
G3ru1a <egidiufarcas@maze.ws>
2020-02-16 11:41:47 +0200

    init

---
+++ src/Providers/MFMServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MAZE\MFM\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+Class MFMServiceProvider extends ServiceProvider{
+
+    public function boot(){
+
+    }
+
+    public function register(){
+
+    }
+
+}

a005ae189087b1a6f4161e8c8f218d4a24b6fd41
G3ru1a <egidiufarcas@maze.ws>
2020-02-16 12:22:23 +0200

    updated provider

--- src/Providers/MFMServiceProvider.php
+++ src/Providers/MFMServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 Class MFMServiceProvider extends ServiceProvider{
 
     public function boot(){
-
+        info('maze/mfm loaded.');
     }
 
     public function register(){
